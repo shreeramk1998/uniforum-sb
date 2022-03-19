a764286784f01e735b169fa2b5eb0f46164cbc77
Shreeram Kulkarni <53758190+shreeramk1998@users.noreply.github.com>
2022-03-20 02:58:13 +0530

    temporary for :compileJava

--- src/main/java/com/uniforum/alpha/controller/PostController.java
+++ src/main/java/com/uniforum/alpha/controller/PostController.java
@@ -1,10 +1,11 @@
 package com.uniforum.alpha.controller;
 
-import java.awt.PageAttributes.MediaType;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,21 +17,21 @@ import com.uniforum.alpha.service.IPostService;
 
 @RestController("/")
 public class PostController {
-
-	@Autowired
-	private IPostService postService;
-
-	@GetMapping(path = "getPosts", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Post>> getPosts(){
-		return new ResponseEntity<List<Post>>(postService.getPosts(), HttpStatus.OK);
-	}
-	
-	@PostMapping(path = "savePost", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Post> savePost(@RequestBody Post post){
-		
-		return new ResponseEntity<Post>(postService.savePost(post),HttpStatus.CREATED);
-	}
-	
+//
+//	@Autowired
+//	private IPostService postService;
+//
+//	@GetMapping(path = "getPosts", produces = MediaType.APPLICATION_JSON_VALUE)
+//	public ResponseEntity<List<Post>> getPosts(){
+//		return new ResponseEntity<List<Post>>(postService.getPosts(), HttpStatus.OK);
+//	}
+//	
+//	@PostMapping(path = "savePost", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//	public ResponseEntity<Post> savePost(@RequestBody Post post){
+//		
+//		return new ResponseEntity<Post>(postService.savePost(post),HttpStatus.CREATED);
+//	}
+//	
 }
 
 
