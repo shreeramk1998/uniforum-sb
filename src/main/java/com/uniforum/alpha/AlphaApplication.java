9d3d9f1b55fe148d5ef4025c7a8456122d8679ce
Shreeram Kulkarni <53758190+shreeramk1998@users.noreply.github.com>
2022-03-19 23:44:52 +0530

    code clean

--- src/main/java/com/uniforum/alpha/AlphaApplication.java
+++ src/main/java/com/uniforum/alpha/AlphaApplication.java
@@ -3,8 +3,6 @@ package com.uniforum.alpha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.Data;
-
 @SpringBootApplication
 public class AlphaApplication {
 
