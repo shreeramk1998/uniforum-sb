adf48240863cea17e63af82ff30d7feef65007f6
Shreeram Kulkarni <53758190+shreeramk1998@users.noreply.github.com>
2022-04-26 23:54:04 +0530

    comment testcase

--- src/test/java/com/uniforum/alpha/AlphaApplicationTests.java
+++ src/test/java/com/uniforum/alpha/AlphaApplicationTests.java
@@ -1,13 +1,13 @@
-package com.uniforum.alpha;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AlphaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.uniforum.alpha;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AlphaApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
